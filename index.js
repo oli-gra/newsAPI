e75be4abfe8d1cc4a7269254022b5a2debe99150
mrsalvini <dsgmcosta@gmail.com>
2019-05-06 16:54:20 +0100

    nicer front end

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ const userDiv = document.querySelector('#user-panel')
 
 const renderNewsBlock = (article) => {
   const newsBlock = document.createElement('article')
+  newsBlock.className = "uk-child-width-1@l uk-margin"
+  newsBlock.setAttribute("uk-grid", "")
   newsBlock.innerHTML = `
-  <h2>${article.title}</h2>
-  <h5>${article.author}</h5>
-  <img src="${article.urlToImage}">
-  <p>${article.description}</p>
+    <div>
+      <div class="uk-card uk-card-default">
+        <div class="uk-card-media-top">
+          <img src="${article.urlToImage}" alt="">
+        </div>
+        <div class="uk-card-body">
+          <h3 class="uk-card-title">${article.title}</h3>
+          <p class=".uk-article-meta">${article.description}</p>
+          <h6 class="uk-article-meta">${article.author}</h6>
+        </div>
+        <div class="uk-card-footer uk-flex uk-flex-between">
+          <a href="#" class="uk-button uk-button-text">Read more</a>
+          <a href="#" class="uk-button uk-button-text">Like</a>
+          <a href="#" class="uk-button uk-button-text">Dislike</a>
+        </div>
+      </div>
+    </div>
   `
   newsDiv.append(newsBlock)
 }
@@ -19,7 +34,6 @@ const renderNewsBlock = (article) => {
 const renderAllNews = () => {
   articles.forEach(renderNewsBlock)
 }
-
 
 const init = () => {
   renderAllNews()
