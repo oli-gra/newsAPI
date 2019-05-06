3d7fced681c88b1a972ee2738994818341fed168
oli-gra <oliver.grant@gmail.com>
2019-05-06 21:29:01 +0100

    seed & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  get 'users/:id', to: 'users#show'
   get 'users/new'
   get 'users/create'
   get 'news/index'
-  get 'news/show'
+  get 'news/:id', to: 'news#show'
   get 'news/new'
   get 'news/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
 User.create(email: 'spam@gmail.com')
-New.create(source: 'https://techcrunch.com/2019/05/05/thunes-raises-10m/',
-   title: 'Thunes raises $10M to make financial services more accessible in emerging markets',
-   image: 'https://techcrunch.com/wp-content/uploads/2019/05/thunes.jpg?w=750',
-   published: '2019-05-06T01:36:03Z')
 React.create(new_id:1,user_id:1,like:false,report:false,
    alt_headline: 'Tunes has a branding problem')
 Follow.create(user_id:1,follow_id:1)
+
+url = 'https://newsapi.org/v2/top-headlines?'\
+      'country=us&'\
+      'apiKey=976ace69bfbe48e69375bd3929688817'
+req = open(url)
+resp = JSON.parse(req.read)
+resp['articles'].each do|r|
+   New.create(source: r['url'], title: r['title'], image: r['urlToImage'], published: r['publishedAt'])
+end
