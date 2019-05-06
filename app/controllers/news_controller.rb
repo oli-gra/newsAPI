69ca0e75619488ddc5ee3b9c41b8fa1bd1093f4a
oli-gra <oliver.grant@gmail.com>
2019-05-06 12:13:45 +0100

    http://localhost:3000/news/index

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,12 @@
 class NewsController < ApplicationController
   def index
+    news = New.all
+    render json: news
   end
 
   def show
+    news = New.find_by(id:params[:id])
+    render json: news
   end
 
   def new
